297d4e073b3520b2d4195204a88c4e15b1ddd136
Mohammad Barati <m.mohammadbarati.b@gmail.com>
2022-01-07 03:03:52 +0330

    add all the files

---
+++ README.md
@@ -0,0 +1,7 @@
+<div>
+  <img src="./images/page.PNG" />
+  <h1>WhatsApp Clone</h1>
+  <p>A WhatsApp clone with pure javascript.</p>
+  <p>Note: Just the three tabs (Chats - Status - Calls) work.</p>
+  <p>Edit the codes and let me know if you have more knowledge or better ideas.</p>
+</div>
---
+++ script.js
@@ -0,0 +1,28 @@
+const $ = document;
+const tab = $.querySelector(".header__tabs");
+const tabBtns = tab.querySelectorAll("button");
+const btnBorder = tab.querySelector(".btnBorder");
+const main = $.querySelector("main");
+const containerIcon = $.querySelector(".container__icon i");
+const containerPenIcon = $.querySelector(".container__pen-icon");
+main.style.left = "0";
+
+const switchPages = (index, left, iconClassName, penIconBottom) => {
+  main.style.left = `${left}rem`;
+  btnBorder.style.width = `${tabBtns[index].clientWidth}px`;
+  btnBorder.style.left = `${tabBtns[index].offsetLeft}px`;
+  tab.querySelector("button.active").classList.remove("active");
+  tabBtns[index].classList.add("active");
+  containerIcon.className = iconClassName;
+  containerPenIcon.style.bottom = `${penIconBottom}rem`;
+};
+
+tabBtns[0].onclick = () => {
+  switchPages(0, 0, "fas fa-comment-alt", 1);
+};
+tabBtns[1].onclick = () => {
+  switchPages(1, -18, "fas fa-camera", 4.5);
+};
+tabBtns[2].onclick = () => {
+  switchPages(2, -37, "fas fa-phone-alt", 1);
+};
